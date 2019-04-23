cc1d57459717f423ac4f3ed78d9672bcfca237d2
Jrigtrup <46531126+Jrigtrup@users.noreply.github.com>
2019-04-22 22:15:34 -0600

    Add files via upload

---
+++ golfAction.js
@@ -0,0 +1,43 @@
+
+
+class PlayerGenerator{
+    constructor(){
+        this.playerCollection = [];
+        this.myarray = [0];
+        this.nameArray = [""];
+    }
+
+    add(id, name){
+        let myname;
+        let randum = Math.floor(Math.random() * this.nameArray.length);
+        if(name === undefined){
+            myname = this.nameArray[randum];
+        }
+        else{
+            myname = name;
+        }
+        this.playerCollection.push(new Player(id, myname, this.myarray));
+        this.nameArray.splice(randum, 1);
+    }
+    delplayer(id){
+        for(let p = 0; p < this.playerCollection.length; p++){
+            if(this.playerCollection[p].id === id){
+                this.playerCollection.splice(p, 1);
+            }
+        }
+    }
+
+}
+
+class Player{
+    constructor(id, name, scores) {
+        this.scoreArray = scores;
+        this.name = name;
+        this.id = id;
+    }
+}
+
+let playgen = new PlayerGenerator();
+
+
+
